ef32956331dd5af6595cc6d1c02981ba4003fbfc
Raviteja Gandam <gandamraviteja@gmail.com>
2024-08-06 15:21:32 +0530

    First Commit

---
+++ tinder-ai/src/test/java/com/ravo/tinder_ai/TinderAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravo.tinder_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
